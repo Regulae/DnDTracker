a414d87e9f88f5d46a2951a186df2e97d696690b
Regulae <engelhardt@noiseag.ch>
2020-09-08 10:04:12 +0200

    Mock data and layout changed

--- app/src/main/java/com/example/dndtracker/CharacterAdapter.kt
+++ app/src/main/java/com/example/dndtracker/CharacterAdapter.kt
@@ -1,6 +1,11 @@
 package com.example.dndtracker
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.RelativeSizeSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -26,8 +31,9 @@ class CharacterAdapter(var characters: MutableList<Character>, val context: Cont
         return index.toLong()
     }
 
+    @SuppressLint("SetTextI18n")
     override fun getView(index: Int, oldView: View?, viewGroup: ViewGroup?): View {
-        var view: View
+        val view: View
         if (oldView == null) { //check if we get a view to recycle
             view = layoutInflater.inflate(R.layout.character_cell, null)
         } else { //if yes, use the oldview
@@ -37,6 +43,18 @@ class CharacterAdapter(var characters: MutableList<Character>, val context: Cont
             getItem(index) //get the data for this index
         view.characterName.text = character.characterName
         view.playerName.text = character.playerName
+        val initiative : String = "Initiative\n" + character.initative
+        val ssInitiative: SpannableString = SpannableString(initiative)
+        ssInitiative.setSpan(RelativeSizeSpan(1.5f), 11, ssInitiative.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        view.initiative.text = ssInitiative
+        val armourClass : String = "AC\n" + character.armourClass
+        val ssArmourClass: SpannableString = SpannableString(armourClass)
+        ssArmourClass.setSpan(RelativeSizeSpan(1.5f), 3, ssArmourClass.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        view.armourClass.text = ssArmourClass
+        val health : String = "HP\n" + character.currentHealth + "/" + character.maxHealth
+        val ssHealth: SpannableString = SpannableString(health)
+        ssHealth.setSpan(RelativeSizeSpan(1.5f), 3, ssHealth.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        view.health.text = ssHealth
 
         return view
     }
--- app/src/main/java/com/example/dndtracker/MainActivity.kt
+++ app/src/main/java/com/example/dndtracker/MainActivity.kt
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class Character(val characterName: String, val playerName: String, val maxHealth: Short, var currentHealth: Short, var initative: Short) {}
+class Character(
+    val characterName: String,
+    val playerName: String,
+    var maxHealth: Short,
+    var currentHealth: Short,
+    var initative: Short,
+    var armourClass: Short,
+    var speed: Short,
+    var level: Short,
+    val npc: Boolean
+) {}
 
 class MainActivity : AppCompatActivity() {
 
     var characters = mutableListOf<Character>(
-            Character("Fjord", "Travis", 100, 100, 20),
-            Character("Jester", "Laura", 80, 80, 15)
+        Character("Fjord", "Travis", 112, 112, 0, 17, 30, 10, false),
+        Character("Jester", "Laura", 92, 92, 4, 18, 30, 10, false),
+        Character("Yasha Nydoorin", "Ashley", 112, 112, 2, 17, 40, 10, false),
+        Character("Beauregard", "Marisha", 85, 85, 5, 20, 50, 10, false),
+        Character("Caleb", "Liam", 65, 65, 1, 15, 30, 10, false),
+        Character("Caduceus", "Taliesin", 87, 87, 1, 18, 30, 10, false),
+        Character("Nott the Brave", "Sam", 81, 81, 5, 18, 35, 10, false)
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
